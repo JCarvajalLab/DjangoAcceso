c00c2764d22099884ec90dae66cd9fe14960d443
JCarvajalLab <carvajal.jordan94@gmail.com>
2026-06-01 19:08:17 -0400

    se agrega boostrap a templates

--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from tienda.forms import LoginForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('', include('tienda.urls')),
-
-    path(
-        'login/',
-        auth_views.LoginView.as_view(),
-        name='login'
-    ),
-
-    path(
-        'logout/',
-        auth_views.LogoutView.as_view(),
-        name='logout'
-    ),
+    path('login/', auth_views.LoginView.as_view(authentication_form=LoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
